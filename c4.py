431f676d5b9d56f8646b78d6c17018fd8ac905f2
Aidan Eickhoff <A.J.Eickhoff@student.tudelft.nl>
2025-01-14 16:09:26 +0100

    removed old code

--- c4.py
+++ c4.py
@@ -1,28 +1,7 @@
 import tkinter
 import numpy as np
-import random
-from rotation_input import Rotation_input
 from input_panel import Input_panel
 from bloch import BlochVisualizer
-
-class Turn:
-    def __init__(self, move_number, color):
-        self.move_number = move_number
-        self.color = color
-        self.type = ""
-
-class SuperTurn(Turn):
-    def __init__(self, move_number, color, distribution=np.zeros(7)):
-        super().__init__(move_number, color)
-        self.distribution = distribution # -> array len 7 of floats
-        self.type = "super"
-
-class EntangleTurn(Turn):
-    def __init__(self, move_number, color, mapping, linked_turn):
-        super().__init__(move_number, color)
-        self.mapping = mapping # -> array len 7 of ints
-        self.linked_turn = linked_turn
-        self.type = "entanglement"
 
 class BoardState():
     def __init__(self):
@@ -72,70 +51,16 @@ class BoardState():
         self.last_collapsed_move = target_turn
         return new_board
 
-
-    # takes a list of moves and a starting board state -- if starting state is none use the board 
-    # calculates the resulting collapsed state. If None supplied as starting state, auto-update game state
-    def collapse_moves(self, moves: list, starting_board: np.ndarray|None = None) -> np.ndarray:
-        # starting state
-        curr_board = starting_board if starting_board is not None else self.board
-        # previous columns 
-        prev_move_cols = []
-        # pass through all the moves which need to be collapsed
-        for move in moves:
-            match move.type:
-                # superposition move logic
-                case "super":
-                    rand = random.random()
-                    count = move.distribution[0]
-                    for col in range(0, 7):
-                        # if true, we have decided randomly on a collapsed superposition, and we save it
-                        if rand < count:
-                            prev_move_cols.append(col)
-                            curr_board = self.add_to_col(col, move.color, curr_board=curr_board)
-                            break
-                        if col == 7:
-                            # SHOULD NEVER GET HERE IF PROBABILITIES ARE 1
-                            return 0
-                        count += move.distribution[col + 1]
-                # entanglement move logic
-                case "entanglement":
-                    entangled_col = move.mapping[prev_move_cols[move.linked_turn - self.last_collapsed_move]]
-                    curr_board = self.add_to_col(entangled_col, move.color, curr_board=curr_board)
-                case _:
-                    print("ERROR")
-        return curr_board
-        
-
-    def add_move(self, dist: np.ndarray, is_entanglement_move: bool, target_turn: int = 0):
-        if not is_entanglement_move:
-            self.moves.append(SuperTurn(move_number=self.total_move_number, distribution=dist, color=self.red_turn))
-        else:
-            if target_turn < self.last_collapsed_move or self.moves[target_turn].type != "super":
-                print("Not a valid target turn")
-                return
-            self.moves.append(EntangleTurn(move_number=self.total_move_number, mapping=dist, linked_turn=target_turn, color=self.red_turn))
-        self.red_turn = not self.red_turn
-
-
 class tkinterHandler():
     def __init__(self):
         self.main_window = tkinter.Tk()
-        # Tkinter interactable widgets
-        self.input_move_type = tkinter.IntVar()
 
-        # board size & place information
-        self.board_width, self.board_height = 1280, 480
-        self.start_board_x_pos = (self.board_width / 2 - (self.board_height * 8 / 12))
-        self.grid_width =  self.board_height / 7
         # create drawable canvas
-        self.canvas = tkinter.Canvas(self.main_window, width=self.board_width, height=self.board_height)
-        self.canvas.create_rectangle(0, 0, self.board_width, self.board_height, fill="#faa", outline="#faa")
         self.bloch_visualizer = BlochVisualizer(self.main_window) #constructor performs .pack()
         self.input_panel = Input_panel(self.main_window, self.add_move) #constructor performs .pack()
 
         self.bloch_visualizer.container.pack(side=tkinter.LEFT)
         self.input_panel.container.pack(side=tkinter.RIGHT)
-
 
         self.board_state: BoardState = BoardState()
 
@@ -144,42 +69,6 @@ class tkinterHandler():
         self.main_window.mainloop()
 
 
-    def get_pos(self, col: int, row: int) -> tuple[int, int]:
-        return (self.start_board_x_pos + col * self.grid_width + 5, row * self.grid_width + 5)
-
-
-    def fill_piece(self, col: int, row: int, fill):
-        offx, offy = self.get_pos(col, row)
-        self.canvas.create_oval(offx, offy, offx + self.grid_width - 10, offy + self.grid_width - 10, fill=fill, outline="#faa")
-
-    def create_board_num(self, col: int, row: int, text):
-        offx, offy = self.get_pos(col, row)
-        self.canvas.create_text(offx + (self.grid_width - 10)/2, offy + (self.grid_width - 10)/2, text=text, font=('Helvetica 15 bold'))
-
-
-    def add_move(self) -> BoardState:
-        text_input = self.input_panel.entanglement_input.get(1.0, "end-1c") 
-        match self.input_move_type.get():
-            # superposition case
-            case 0:
-                # array = np.array(list(map(float, text_input.split(','))))
-                # array *= 1 / np.sum(array)
-                self.board_state.add_move(probs, False)
-            # entanglement case
-            case 1:
-                target_turn = int(self.target_turn_number.get(1.0, "end-1c"))
-                array = np.array(list(map(int, text_input.split(','))))
-                self.board_state.add_move(array, True, target_turn=target_turn)
-
-    def collapse(self, event):
-        new_board = self.board_state.collapse_event()
-        for col in range(0, 7):
-            for row in range(0, 6):
-                if new_board[col][row] != 0:
-                    self.fill_piece(col, row, "#f00" if (new_board[col][row] == 1) else "#ff0")
-        self.board_state.board = new_board
-
-
-
-tkinter_handler = tkinterHandler()
-tkinter_handler.show_window()
+if __name__ == "__main__":
+    tkinter_handler = tkinterHandler()
+    tkinter_handler.show_window()
