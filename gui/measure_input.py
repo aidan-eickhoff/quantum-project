f1142d0e2d89526d047afd96cd1b9f421b051fce
sdrent <S.Drent@student.tudelft.nl>
2025-01-22 12:04:41 +0100

    made measure input consistent with other inputs

--- gui/measure_input.py
+++ gui/measure_input.py
@@ -13,24 +13,24 @@ class Measure_input():
         self.cell_y = tkinter.StringVar()
 
 
-        self.cell_label = tkinter.Label(self.container,text='Measurement move')
-        self.cell_label.grid(row=0,column=0, columnspan=2)
+        self.title_label = tkinter.Label(self.container,text='Measurement move')
+        self.title_label.grid(row=0,column=0, columnspan=6)
 
-        self.cell_label = tkinter.Label(self.container,text='Measuring vector (X,Y,Z)')
-        self.cell_label.grid(row=1,column=0)
-        self.meas_x_input = tkinter.Entry(self.container, textvariable=self.meas_x)
-        self.meas_x_input.grid(row=2,column=0)
-        self.meas_y_input = tkinter.Entry(self.container, textvariable=self.meas_y)
-        self.meas_y_input.grid(row=3,column=0)
-        self.meas_z_input = tkinter.Entry(self.container, textvariable=self.meas_z)
-        self.meas_z_input.grid(row=4,column=0)
+        self.meas_label = tkinter.Label(self.container,text='Measuring vector (X,Y,Z)', justify="left", anchor="w")
+        self.meas_label.grid(row=1,column=0, columnspan=6, sticky = "w")
+        self.meas_x_input = tkinter.Entry(self.container, textvariable=self.meas_x, width=13)
+        self.meas_x_input.grid(row=2,column=0, columnspan=2)
+        self.meas_y_input = tkinter.Entry(self.container, textvariable=self.meas_y, width=13)
+        self.meas_y_input.grid(row=2,column=2, columnspan=2)
+        self.meas_z_input = tkinter.Entry(self.container, textvariable=self.meas_z, width=13)
+        self.meas_z_input.grid(row=2,column=4, columnspan=2)
 
-        self.cell_label = tkinter.Label(self.container,text='Cell (X,Y)')
-        self.cell_label.grid(row=1,column=1)
+        self.cell_label = tkinter.Label(self.container,text='Cell (X,Y)', justify="left", anchor="w")
+        self.cell_label.grid(row=3,column=0, columnspan=6, sticky = "w")
         self.cell_x_input = tkinter.Entry(self.container, textvariable=self.cell_x)
-        self.cell_x_input.grid(row=2,column=1)
+        self.cell_x_input.grid(row=4,column=0, columnspan=3)
         self.cell_y_input = tkinter.Entry(self.container, textvariable=self.cell_y)
-        self.cell_y_input.grid(row=3,column=1)
+        self.cell_y_input.grid(row=4,column=3, columnspan=3)
 
 
         self.container.pack()
